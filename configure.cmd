81248db31f7744dd9ca0fc7d1f85ae868858bcdc
KO Myung-Hun <komh@chollian.net>
2013-02-03 12:07:45 +0900

    Add configure.cmd

---
+++ configure.cmd
@@ -0,0 +1,12 @@
+extproc sh
+
+export ac_executable_extensions=".exe"
+export CPPFLAGS="-D__ST_MT_ERRNO__"
+export CFLAGS="-O2 -Zomf -Zmt"
+export LDFLAGS="-Zcrtdll -Zlinker /exepack:2 -Zlinker /pm:vio -Zstack 0x6000"
+export RANLIB="echo"
+export AR="emxomfar"
+./configure --prefix=/usr \
+            --infodir=/usr/share/info \
+            --mandir=/usr/share/man \
+            --without-included-gettext
