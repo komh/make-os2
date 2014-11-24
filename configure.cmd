6c1fbd9b52b0c93e474637b36b34fc303f469445
KO Myung-Hun <komh@chollian.net>
2014-11-24 12:55:14 +0900

    Revert "configure.cmd: remove -Zcrtdll from LDFLAGS"
    This reverts commit e7f46231c0ea0b60f68f841719784e2eda6964f7.
    
    commit e7f462 has no effects at all.
    
        modified:   configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -3,7 +3,7 @@ extproc sh
 export ac_executable_extensions=".exe"
 export CPPFLAGS="-D__ST_MT_ERRNO__"
 export CFLAGS="-O2 -Zomf -Zmt"
-export LDFLAGS="-Zlinker /exepack:2 -Zlinker /pm:vio -Zstack 0x6000"
+export LDFLAGS="-Zcrtdll -Zlinker /exepack:2 -Zlinker /pm:vio -Zstack 0x6000"
 export RANLIB="echo"
 export AR="emxomfar"
 ./configure --prefix=/usr \
