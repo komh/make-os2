be4ac42f992ecc39564c6187b8a34998f1ebb977
KO Myung-Hun <komh@chollian.net>
2014-12-12 10:45:54 +0900

    configure.cmd: fix warning of emxomfld in WLINK mode
    /exepack:2 is ignored in WLINK mode.
    
    -----
    emxomfld: warning: ignoring ilink option '/exepack:2'
    -----
    
        modified:   configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -3,7 +3,11 @@ extproc sh
 export ac_executable_extensions=".exe"
 export CPPFLAGS="-D__ST_MT_ERRNO__"
 export CFLAGS="-O2 -Zomf -Zmt"
-export LDFLAGS="-Zcrtdll -Zlinker /exepack:2 -Zlinker /pm:vio -Zstack 0x6000"
+if test x"$EMXOMFLD_TYPE" = xWLINK || test x"$EMXOMFLD_TYPE" = xwlink ; then
+  export LDFLAGS="-Zcrtdll -Zlinker /pm:vio -Zstack 0x6000"
+else
+  export LDFLAGS="-Zcrtdll -Zlinker /exepack:2 -Zlinker /pm:vio -Zstack 0x6000"
+fi
 export RANLIB="echo"
 export AR="emxomfar"
 ./configure --prefix=/usr \
