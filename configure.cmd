1914f2d6612808fcd9a5687c2aa2f65d700a9c0b
KO Myung-Hun <komh78@gmail.com>
2026-01-17 20:16:40 +0900

    configure.cmd: update
    1. Set prefix to /@unixroot/usr/local
    2. Return the correct exit code
    
        modified:   configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -8,13 +8,17 @@ test -f "$d/$n." || { echo "\`$d/$n' not found !!!"; exit 1; }
 export ac_executable_extensions=".exe"
 export CPPFLAGS="-D__ST_MT_ERRNO__"
 export CFLAGS="-O2 -Zomf -Zmt"
-if test x"$EMXOMFLD_TYPE" = xWLINK || test x"$EMXOMFLD_TYPE" = xwlink ; then
-  export LDFLAGS="-Zcrtdll -Zlinker /pm:vio -Zstack 0x6000"
-else
-  export LDFLAGS="-Zcrtdll -Zlinker /exepack:2 -Zlinker /pm:vio -Zstack 0x6000"
+
+LDFLAGS="-Zcrtdll -Zstack 0x6000 -Zhigh-mem -Zlinker /pm:vio"
+if test x"$EMXOMFLD_TYPE" != xWLINK -a x"$EMXOMFLD_TYPE" != xwlink ; then
+  LDFLAGS="$LDFLAGS -Zlinker /exepack:2"
 fi
+export LDFLAGS
+
 export RANLIB="echo"
 export AR="emxomfar"
 
-opts=""
+opts="--prefix=/@unixroot/usr/local"
+
 "$d/$n" $opts "$@" 2>&1 | tee "$n.log"
+exit ${PIPESTATUS[0]}
